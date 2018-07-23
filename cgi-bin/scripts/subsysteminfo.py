c710e9575b42ab3046cb17777b238cb75f615ef8
sasakinzm <sasakinzm@gmail.com>
2018-07-23 10:56:38 +0000

    add media-type method

--- cgi-bin/scripts/subsysteminfo.py
+++ cgi-bin/scripts/subsysteminfo.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 class interfaceinfo:
-    def __init__(self, name, admin_state, link_state, speed, description, lag_group, lag_member):
+    def __init__(self, name, admin_state, link_state, speed, description, lag_group, lag_member, media_type):
         self.name = name
         self.admin_state = admin_state
         self.link_state = link_state
@@ -10,6 +10,7 @@ class interfaceinfo:
         self.description = description
         self.lag_group = lag_group
         self.lag_member = lag_member
+        self.media_type = media_type
 
 class bgpinfo:
     def __init__(self, addr, peer_type, state, asn, rcvroutes, advroutes, peer_description):
